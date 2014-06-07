bba2a082b2a1b6f9b141543c0f0f8571a76ddf42
denvdmj <denvdmj@gmail.com>
2014-06-07 07:23:08 +0400

    Refactoring of function genBitmapImage

--- canvas2image.js
+++ canvas2image.js
@@ -84,110 +84,111 @@ var Canvas2Image = function () {
 	 * create bitmap image
 	 * 按照规则生成图片响应头和响应体
 	 */
-	var genBitmapImage = function (data) {
-		var imgHeader = [],
-			imgInfoHeader = [];
-		
-		var width = data.width,
-			height = data.height;
+	var genBitmapImage = function (oData) {
 
-		imgHeader.push(0x42); // 66 -> B
-		imgHeader.push(0x4d); // 77 -> M
+		//
+		// BITMAPFILEHEADER: http://msdn.microsoft.com/en-us/library/windows/desktop/dd183374(v=vs.85).aspx
+		// BITMAPINFOHEADER: http://msdn.microsoft.com/en-us/library/dd183376.aspx
+		//
 
-		var fsize = width * height * 3 + 54; // header size:54 bytes
-		imgHeader.push(fsize % 256); // r
-		fsize = Math.floor(fsize / 256);
-		imgHeader.push(fsize % 256); // g
-		fsize = Math.floor(fsize / 256);
-		imgHeader.push(fsize % 256); // b
-		fsize = Math.floor(fsize / 256);
-		imgHeader.push(fsize % 256); // a
+		var biWidth  = oData.width;
+		var biHeight	= oData.height;
+		var biSizeImage = biWidth * biHeight * 3;
+		var bfSize  = biSizeImage + 54; // total header size = 54 bytes
 
-		imgHeader.push(0);
-		imgHeader.push(0);
-		imgHeader.push(0);
-		imgHeader.push(0);
+		//
+		//  typedef struct tagBITMAPFILEHEADER {
+		//  	WORD bfType;
+		//  	DWORD bfSize;
+		//  	WORD bfReserved1;
+		//  	WORD bfReserved2;
+		//  	DWORD bfOffBits;
+		//  } BITMAPFILEHEADER;
+		//
+		var BITMAPFILEHEADER = [
+			// WORD bfType -- The file type signature; must be "BM"
+			0x42, 0x4D,
+			// DWORD bfSize -- The size, in bytes, of the bitmap file
+			bfSize & 0xff, bfSize >> 8 & 0xff, bfSize >> 16 & 0xff, bfSize >> 24 & 0xff,
+			// WORD bfReserved1 -- Reserved; must be zero
+			0, 0,
+			// WORD bfReserved2 -- Reserved; must be zero
+			0, 0,
+			// DWORD bfOffBits -- The offset, in bytes, from the beginning of the BITMAPFILEHEADER structure to the bitmap bits.
+			54, 0, 0, 0
+		];
 
-		imgHeader.push(54); // offset -> 6
-		imgHeader.push(0);
-		imgHeader.push(0);
-		imgHeader.push(0);
+		//
+		//  typedef struct tagBITMAPINFOHEADER {
+		//  	DWORD biSize;
+		//  	LONG  biWidth;
+		//  	LONG  biHeight;
+		//  	WORD  biPlanes;
+		//  	WORD  biBitCount;
+		//  	DWORD biCompression;
+		//  	DWORD biSizeImage;
+		//  	LONG  biXPelsPerMeter;
+		//  	LONG  biYPelsPerMeter;
+		//  	DWORD biClrUsed;
+		//  	DWORD biClrImportant;
+		//  } BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+		//
+		var BITMAPINFOHEADER = [
+			// DWORD biSize -- The number of bytes required by the structure
+			40, 0, 0, 0,
+			// LONG biWidth -- The width of the bitmap, in pixels
+			biWidth & 0xff, biWidth >> 8 & 0xff, biWidth >> 16 & 0xff, biWidth >> 24 & 0xff,
+			// LONG biHeight -- The height of the bitmap, in pixels
+			biHeight & 0xff, biHeight >> 8  & 0xff, biHeight >> 16 & 0xff, biHeight >> 24 & 0xff,
+			// WORD biPlanes -- The number of planes for the target device. This value must be set to 1
+			1, 0,
+			// WORD biBitCount -- The number of bits-per-pixel, 24 bits-per-pixel -- the bitmap
+			// has a maximum of 2^24 colors (16777216, Truecolor)
+			24, 0,
+			// DWORD biCompression -- The type of compression, BI_RGB (code 0) -- uncompressed
+			0, 0, 0, 0,
+			// DWORD biSizeImage -- The size, in bytes, of the image. This may be set to zero for BI_RGB bitmaps
+			biSizeImage & 0xff, biSizeImage >> 8 & 0xff, biSizeImage >> 16 & 0xff, biSizeImage >> 24 & 0xff,
+			// LONG biXPelsPerMeter, unused
+			0,0,0,0,
+			// LONG biYPelsPerMeter, unused
+			0,0,0,0,
+			// DWORD biClrUsed, the number of color indexes of palette, unused
+			0,0,0,0,
+			// DWORD biClrImportant, unused
+			0,0,0,0
+		];
 
-		// info header
-		imgInfoHeader.push(40); // info header size
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(0);
+		var iPadding = (4 - ((biWidth * 3) % 4)) % 4;
 
-		// 横向info
-		var _width = width;
-		imgInfoHeader.push(_width % 256);
-		_width = Math.floor(_width / 256);
-		imgInfoHeader.push(_width % 256);
-		_width = Math.floor(_width / 256);
-		imgInfoHeader.push(_width % 256);
-		_width = Math.floor(_width / 256);
-		imgInfoHeader.push(_width % 256);
+		var aImgData = oData.data;
 
-		// 纵向info
-		var _height = height;
-		imgInfoHeader.push(_height % 256);
-		_height = Math.floor(_height / 256);
-		imgInfoHeader.push(_height % 256);
-		_height = Math.floor(_height / 256);
-		imgInfoHeader.push(_height % 256);
-		_height = Math.floor(_height / 256);
-		imgInfoHeader.push(_height % 256);
-
-		imgInfoHeader.push(1);
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(24); // 24位bitmap
-		imgInfoHeader.push(0);
-
-		// no compression
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(0);
-		imgInfoHeader.push(0);
-
-		// pixel data
-		var dataSize = width * height * 3;
-		imgInfoHeader.push(dataSize % 256);
-		dataSize = Math.floor(dataSize / 256);
-		imgInfoHeader.push(dataSize % 256);
-		dataSize = Math.floor(dataSize / 256);
-		imgInfoHeader.push(dataSize % 256);
-		dataSize = Math.floor(dataSize / 256);
-		imgInfoHeader.push(dataSize % 256);
-
-		// blank space
-		for (var i = 0; i < 16; i ++) {
-			imgInfoHeader.push(0);
-		}
-
-		var padding = (4 - ((width * 3) % 4)) % 4;
-		var imgData = data.data;
 		var strPixelData = '';
-		var y = height;
+		var biWidth4 = biWidth<<2;
+		var y = biHeight;
+		var fromCharCode = String.fromCharCode;
+
 		do {
-			var offsetY = width * (y - 1) * 4;
+			var iOffsetY = biWidth4*(y-1);
 			var strPixelRow = '';
-			for (var x = 0; x < width; x ++) {
-				var offsetX = 4 * x;
-				strPixelRow += String.fromCharCode(imgData[offsetY + offsetX + 2]);
-				strPixelRow += String.fromCharCode(imgData[offsetY + offsetX + 1]);
-				strPixelRow += String.fromCharCode(imgData[offsetY + offsetX]);
+			for (var x = 0; x < biWidth; x++) {
+				var iOffsetX = x<<2;
+				strPixelRow += fromCharCode(aImgData[iOffsetY+iOffsetX+2]) +
+							   fromCharCode(aImgData[iOffsetY+iOffsetX+1]) +
+							   fromCharCode(aImgData[iOffsetY+iOffsetX]);
 			}
-			for (var n = 0; n < padding; n ++) {
+
+			for (var c = 0; c < iPadding; c++) {
 				strPixelRow += String.fromCharCode(0);
 			}
 
 			strPixelData += strPixelRow;
-		} while(-- y);
+		} while (--y);
 
-		return (encodeData(imgHeader.concat(imgInfoHeader)) + encodeData(strPixelData));
+		var strEncoded = encodeData(BITMAPFILEHEADER.concat(BITMAPINFOHEADER)) + encodeData(strPixelData);
 
-	};
+		return strEncoded;
+	};	
 
 	/**
 	 * saveAsImage
